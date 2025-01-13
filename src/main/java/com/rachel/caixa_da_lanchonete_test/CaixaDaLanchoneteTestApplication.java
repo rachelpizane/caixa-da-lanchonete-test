23f964cf42e738bb7f1a556efa06b1d50105b480
Rachel Pizane <rachel.pzm@gmail.com>
2025-01-13 16:30:38 -0300

    first commit

---
+++ src/main/java/com/rachel/caixa_da_lanchonete_test/CaixaDaLanchoneteTestApplication.java
@@ -0,0 +1,13 @@
+package com.rachel.caixa_da_lanchonete_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixaDaLanchoneteTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixaDaLanchoneteTestApplication.class, args);
+	}
+
+}
